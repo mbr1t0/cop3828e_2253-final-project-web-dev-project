ec96680fd11cea86d73ed813be8e97573717e683
Miguel Brito <remil16m@gmail.com>
2025-04-10 18:38:59 -0400

    Update
    Updated code

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+document.getElementById("funFactOne").addEventListener("click", function() {
+    alert("He is a member of the Marching 100 in the percussion section.");
+});
+
+document.getElementById("funFactTwo").addEventListener("click", function() {
+    alert("He is a member of the Marching 100 in the percussion section.");
+});
+
+document.getElementById("funFactThree").addEventListener("click", function() {
+    alert("He is a member of the Marching 100 in the percussion section.");
+});
